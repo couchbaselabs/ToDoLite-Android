2161f3289de99b271c2852e5365a8eff997aa639
Laurent Doguin <laurent.doguin@couchbase.com>
2015-06-02 02:00:53 +0200

    initiate database

--- ToDoLite/src/main/java/com/couchbase/todolite/Application.java
+++ ToDoLite/src/main/java/com/couchbase/todolite/Application.java
@@ -35,28 +35,6 @@ public class Application extends android.app.Application {
     private ToDoLitePreferences preferences;
 
     private void initDatabase() {
-        try {
-
-            Manager.enableLogging(TAG, Log.VERBOSE);
-            Manager.enableLogging(Log.TAG, Log.VERBOSE);
-            Manager.enableLogging(Log.TAG_SYNC_ASYNC_TASK, Log.VERBOSE);
-            Manager.enableLogging(Log.TAG_SYNC, Log.VERBOSE);
-            Manager.enableLogging(Log.TAG_QUERY, Log.VERBOSE);
-            Manager.enableLogging(Log.TAG_VIEW, Log.VERBOSE);
-            Manager.enableLogging(Log.TAG_DATABASE, Log.VERBOSE);
-
-            manager = new Manager(new AndroidContext(getApplicationContext()), Manager.DEFAULT_OPTIONS);
-        } catch (IOException e) {
-            Log.e(TAG, "Cannot create Manager object", e);
-            return;
-        }
-
-        try {
-            database = manager.getDatabase(DATABASE_NAME);
-        } catch (CouchbaseLiteException e) {
-            Log.e(TAG, "Cannot get Database", e);
-            return;
-        }
     }
 
     private Replication.ChangeListener getReplicationChangeListener() {
